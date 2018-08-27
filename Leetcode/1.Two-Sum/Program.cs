477a76e8541dd8967a7fb9f0395922bfb9ace70c
Eugeny Sampir <2004195@gmail.com>
2018-08-27 16:58:23 +0300

    first commit

---
+++ Leetcode/1.Two-Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

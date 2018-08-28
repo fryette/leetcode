9f81470998fac3f9be6442e1e0982f53360c6be8
Eugeny Sampir <2004195@gmail.com>
2018-08-28 20:29:03 +0300

    Resolved 167  Two Sum 2 Input Array Is Sorted Solution

---
+++ Leetcode/LeetCode.Solutions/Low/167.TwoSum2InputArrayIsSortedSolution.cs
@@ -0,0 +1,47 @@
+﻿namespace LeetCode.Solutions.Low
+{
+    public class TwoSum2InputArrayIsSortedSolution
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                var binarySearchResult = BinarySearch(numbers, target - numbers[i], i + 1);
+
+                if (binarySearchResult != -1)
+                {
+                    return new int[] { i + 1, ++binarySearchResult };
+                }
+            }
+
+            return null;
+        }
+
+        private static int BinarySearch(int[] array, int x, int start)
+        {
+            int first = start;
+            int last = array.Length - 1;
+
+            while (first < last)
+            {
+                int mid = first + (last - first) / 2;
+
+                if (x <= array[mid])
+                {
+                    last = mid;
+                }
+                else
+                {
+                    first = mid + 1;
+                }
+            }
+
+            if (array[last] == x)
+            {
+                return last;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Leetcode/LeetCode.Tests/Low/167.TwoSum2InputArrayIsSortedTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Linq;
+using LeetCode.Solutions.Low;
+using Xunit;
+
+namespace LeetCode.Tests.Low
+{
+    public class TwoSum2InputArrayIsSortedTests
+    {
+        [Fact]
+        public void Common_Test()
+        {
+            // Arrange
+            var solution = new TwoSum2InputArrayIsSortedSolution();
+            var expected = new[] { 1, 2 };
+
+            //act
+            var result = solution.TwoSum(new[] { 2, 7, 11, 15 }, 9);
+
+            //Assert
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [Fact]
+        public void Different_positions_should_not_affect_result()
+        {
+            // Arrange
+            var solution = new TwoSum2InputArrayIsSortedSolution();
+            var expected = new[] { 1, 3 };
+
+            //act
+            var result = solution.TwoSum(new[] { 2, 5, 7, 11, 15 }, 9);
+
+            //Assert
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [Fact]
+        public void Duplications_in_array_should_not_affect_result()
+        {
+            // Arrange
+            var solution = new TwoSum2InputArrayIsSortedSolution();
+            var expected = new[] { 1, 3 };
+
+            //act
+            var result = solution.TwoSum(new[] { 2, 2, 7, 11, 15 }, 9);
+
+            //Assert
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+
+        [Fact]
+        public void Right_numbers_at_the_boarder()
+        {
+            // Arrange
+            var solution = new TwoSum2InputArrayIsSortedSolution();
+            var expected = new[] { 2, 3 };
+
+            //act
+            var result = solution.TwoSum(new[] { 5, 25, 75 }, 100);
+
+            //Assert
+            Assert.True(Enumerable.SequenceEqual(expected, result));
+        }
+    }
+}

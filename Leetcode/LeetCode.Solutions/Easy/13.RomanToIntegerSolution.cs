0d086959356951e3329d182fe745f8de888cded4
Eugeny Sampir <2004195@gmail.com>
2018-08-28 16:55:59 +0300

    Resolved 13 roman to integer

---
+++ Leetcode/LeetCode.Solutions/Easy/13.RomanToIntegerSolution.cs
@@ -0,0 +1,52 @@
+﻿namespace LeetCode.Solutions.Easy
+{
+    public class RomanToIntegerSolution
+    {
+        public int RomanToInt(string s)
+        {
+            int GetBasicNumber(char c)
+            {
+                switch (c)
+                {
+                    case 'I': return 1;
+                    case 'V': return 5;
+                    case 'X': return 10;
+                    case 'L': return 50;
+                    case 'C': return 100;
+                    case 'D': return 500;
+                    case 'M': return 1000;
+                }
+
+                return -1;
+            }
+
+            int total = 0;
+            var i = 0;
+            int maxLength = s.Length - 1;
+
+            for (i = 0; i < maxLength; i++)
+            {
+                int num = GetBasicNumber(s[i]);
+
+                int nextNum = GetBasicNumber(s[i + 1]);
+
+                if (num >= nextNum)
+                {
+                    total += num;
+                }
+                else
+                {
+                    total += nextNum - num;
+                    ++i;
+                }
+            }
+
+            if (i == maxLength)
+            {
+                total += GetBasicNumber(s[s.Length - 1]);
+            }
+
+            return total;
+        }
+    }
+}
---
+++ Leetcode/LeetCode.Tests/Easy/13.RomanTointegerTests.cs
@@ -0,0 +1,50 @@
+﻿using LeetCode.Solutions.Easy;
+using Xunit;
+
+namespace LeetCode.Tests.Easy
+{
+    public class RomanTointegerTests
+    {
+        [Fact]
+        public void Common_Test()
+        {
+            // Arrange
+            var solution = new RomanToIntegerSolution();
+            var expected = 4;
+
+            //act
+            var result = solution.RomanToInt("IV");
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Case_with_next_symbol_checking()
+        {
+            // Arrange
+            var solution = new RomanToIntegerSolution();
+            var expected = 1695;
+
+            //act
+            var result = solution.RomanToInt("MDCXCV");
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Should_increment_by_one()
+        {
+            // Arrange
+            var solution = new RomanToIntegerSolution();
+            var expected = 3;
+
+            //act
+            var result = solution.RomanToInt("III");
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}

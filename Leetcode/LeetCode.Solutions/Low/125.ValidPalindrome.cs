ba31640e444f4887a1080aea2d5aeba945c9002f
Eugeny Sampir <2004195@gmail.com>
2018-08-31 17:26:24 +0300

    Resolved 125. Valid Palindrome

---
+++ Leetcode/LeetCode.Solutions/Low/125.ValidPalindrome.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode.Solutions.Low
+{
+    public class ValidPalindromeSolution
+    {
+        //https://leetcode.com/problems/valid-palindrome/description/
+        public bool IsPalindrome(string s)
+        {
+            var j = s.Length - 1;
+            var i = 0;
+
+            while (i < j)
+            {
+                while (i < j && !char.IsLetterOrDigit(s[i])) { ++i; }
+                while (j > i && !char.IsLetterOrDigit(s[j])) { --j; }
+
+                if (char.ToUpperInvariant(s[i]) != char.ToUpperInvariant(s[j]))
+                {
+                    return false;
+                }
+                i++;
+                --j;
+            }
+
+            return true;
+        }
+    }
+}

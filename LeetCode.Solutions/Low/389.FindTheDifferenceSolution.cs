c97f8e087cb2d65498f43af248985f5e693594fe
Eugeny Sampir <2004195@gmail.com>
2018-09-01 16:34:04 +0300

    Resolved 389

---
+++ LeetCode.Solutions/Low/389.FindTheDifferenceSolution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LeetCode.Solutions.Low
+{
+    public class FindTheDifferenceSolution
+    {
+		public char FindTheDifference(string s, string t)
+        {
+            var sum1 = 0;
+            var sum2 = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                sum1 += s[i];
+                sum2 += t[i];
+            }
+
+            sum2 += t[s.Length];
+
+            return (char)(sum2 - sum1);
+        }
+    }
+}

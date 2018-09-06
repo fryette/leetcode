3e67a0c1384662f6cfbe96c8225f1efe6d9ce805
Eugeny Sampir <2004195@gmail.com>
2018-09-06 10:19:24 +0300

    fixes with palindromic algorithm

--- LeetCode.Solutions/Low/9.PalindromeNumberSolution.cs
+++ LeetCode.Solutions/Low/9.PalindromeNumberSolution.cs
@@ -19,5 +19,36 @@
 
             return true;
         }
+
+          // without allocating memory solution
+//        public bool IsPalindrome(int x)
+//        {
+//            if (x < 0)
+//            {
+//                return false;
+//            }
+//
+//            var div = 1;
+//            while (x / div >= 10)
+//            {
+//                div *= 10;
+//            }
+//
+//            while (x != 0)
+//            {
+//                var left = x / div;
+//                var right = x % 10;
+//
+//                if (left != right)
+//                {
+//                    return false;
+//                }
+//
+//                x = (x % div) / 10;
+//                div /= 100;
+//            }
+//
+//            return true;
+//        }
     }
 }

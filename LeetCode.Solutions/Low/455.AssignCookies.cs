d6c4a87dc376ac7f68865ee7be9a661e35e00ac2
Eugeny Sampir <2004195@gmail.com>
2018-11-05 23:59:55 +0300

    added 455 and 461

---
+++ LeetCode.Solutions/Low/455.AssignCookies.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode.Solutions.Low
+{
+    //https://leetcode.com/problems/assign-cookies/description/
+    public class AssignCookiesSolution
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            Array.Sort(g);
+            Array.Sort(s);
+
+            var child = g.Length - 1;
+            var cookie = s.Length - 1;
+            var result = 0;
+
+            while (child >= 0 && cookie >= 0)
+            {
+                if (s[cookie] >= g[child])
+                {
+                    cookie--;
+                    result++;
+                }
+
+                child--;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ LeetCode.Tests/Low/461.HammingDistanceSolution.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using LeetCode.Solutions.Low;
+using Xunit;
+
+namespace LeetCode.Tests.Low
+{
+	public class HammingDistanceTests
+	{
+		[Fact]
+		public void Common_Test()
+		{
+			// Arrange
+			var solution = new HammingDistanceSolution();
+
+			//act
+			var result = solution.HammingDistance(1, 4);
+
+			//Assert
+			Assert.Equal(2, result);
+		}
+
+		[Fact]
+        public void Test_With_Max_Int()
+        {
+            // Arrange
+            var solution = new HammingDistanceSolution();
+
+            //act
+			var result = solution.HammingDistance(4, 2147483647);
+
+            //Assert
+            Assert.Equal(30, result);
+        }
+
+		[Fact]
+        public void Test_With_Identical_Values()
+        {
+            // Arrange
+            var solution = new HammingDistanceSolution();
+
+            //act
+            var result = solution.HammingDistance(4, 4);
+            
+            //Assert
+            Assert.Equal(0, result);
+        }
+	}
+}

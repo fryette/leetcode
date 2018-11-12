ecb6144a93a4d1522a928dd3bbad32a1a665e081
Eugeny Sampir <2004195@gmail.com>
2018-11-12 12:06:15 +0300

    Added 852. Peak Index in a Mountain Array

---
+++ LeetCode.Solutions/Low/852.PeakIndexInAMountainArray.cs
@@ -0,0 +1,27 @@
+namespace LeetCode.Solutions.Low
+{
+    public class PeakIndexInAMountainArraySolution
+    {
+        public int PeakIndexInMountainArray(int[] a)
+        {
+            var first = 0;
+            var last = a.Length;
+
+            while (first < last)
+            {
+                var mid = first + (last - first) / 2;
+
+                if (a[mid] < a[mid + 1])
+                {
+                    first = mid + 1;
+                }
+                else
+                {
+                    last = mid;
+                }
+            }
+
+            return last;
+        }
+    }
+}

b15c20b5bf3861d9dccddf70e64d5ab1c5d950fa
Eugeny Sampir <2004195@gmail.com>
2018-11-14 11:48:12 +0300

    Added 374 and 81

---
+++ LeetCode.Solutions/Low/374.GuessNumberHigherOrLower.cs
@@ -0,0 +1,36 @@
+using System;
+
+namespace LeetCode.Solutions.Low
+{
+    //https://leetcode.com/problems/guess-number-higher-or-lower
+    public class GuessNumberHigherOrLowerSolution
+    {
+        public int GuessNumber(int n)
+        {
+// Java implementation            
+//            int left = 0;
+//            int right = n;
+//        
+//            while(left<right)
+//            {
+//                int mid = left + (right-left)/2;
+//                int guessResult = guess(mid);
+//                if(guessResult==-1)
+//                {
+//                    right = mid - 1;
+//                }
+//                else if(guessResult==1)
+//                {
+//                    left=mid+1;
+//                }
+//                else
+//                {
+//                    return mid;
+//                }
+//            }
+//        
+//            return right;
+            throw new NotImplementedException();
+        }
+    }
+}

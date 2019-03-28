dda2824451a546ba8bef4248c3650dfb75501830
Eugeny Sampir <2004195@gmail.com>
2019-03-28 09:38:46 +0300

    Added 520. Detect Capital

---
+++ LeetCode.Solutions/Low/520.DetectCapital.cs
@@ -0,0 +1,21 @@
+﻿namespace LeetCode.Solutions.Low
+{
+    public class DetectCapital
+    {
+        public bool DetectCapitalUse(string word)
+        {
+            var firstIsUpper = word[0] <= 90;
+            var upperCount = firstIsUpper ? 1 : 0;
+
+            for (var i = 1; i < word.Length; i++)
+            {
+                if (word[i] <= 90)
+                {
+                    upperCount++;
+                }
+            }
+
+            return firstIsUpper && upperCount == 1 || upperCount == word.Length || upperCount == 0;
+        }
+    }
+}
---
+++ LeetCode.Tests/Low/520.DetectCapital.cs
@@ -0,0 +1,88 @@
+﻿using LeetCode.Solutions.Low;
+using Xunit;
+
+namespace LeetCode.Tests.Low
+{
+    public class DetectCapitalTests
+    {
+        [Fact]
+        public void First_Character_Upper_Other_Lower()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("Asupertest");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void All_Characters_Upper()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("USA");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void All_Characters_Lower()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("usa");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void One_Character_Upper_Case()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("A");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void One_Character_Lower_Case()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("a");
+
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void First_Character_Upper_Last_Character_Upper()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("AsupertestB");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void First_Character_Upper_Center_Character_Upper()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("AsupeRtestb");
+
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void Last_Character_Upper()
+        {
+            var solution = new DetectCapital();
+
+            var result = solution.DetectCapitalUse("asupertestB");
+
+            Assert.False(result);
+        }
+    }
+}
